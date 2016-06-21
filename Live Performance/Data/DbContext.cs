7f71b0051fc852a72d1ab275555e4bd460b2da74
Heijkoop, Marie-Rose M.M <m.heijkoop@student.fontys.nl>
2016-06-21 22:54:40 +0200

    Added project

---
+++ Live Performance/Data/DbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Live_Performance.Data
+{
+    /// <summary>
+    /// Base database class where the database connection will be created.
+    /// </summary>
+    public abstract class DbContext
+    {
+        public static string ConnectionString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)" +
+                                                "(HOST = fhictora01.fhict.local)(PORT = 1521)))(CONNECT_DATA=(SERVER=DEDICATED)" +
+                                                "(SERVICE_NAME=fhictora)));User ID = dbi334051; PASSWORD=a112358aB";
+
+        /// <summary>
+        /// Method that creates the database connection.
+        /// </summary>
+        /// <returns>connection</returns>
+        public static OracleConnection CreateConnection()
+        {
+            OracleConnection connection = new OracleConnection(ConnectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
